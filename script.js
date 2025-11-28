30ea830c515bfb8b7059f693e77a36d92c5cc359
musheghwork <musheghwork@gmail.com>
2025-11-28 19:32:44 +0400

    implement city toggle functionality and update footer styles for active state

--- script.js
+++ script.js
@@ -129,10 +129,10 @@ noBtn.addEventListener("click", () => {
 const city = document.getElementById("city");
 const cont = document.querySelectorAll(".foot-cont-three a");
 
-city.addEventListener("click", openCont);
-
-function openCont() {
+city.addEventListener("click", toggleCont);
+function toggleCont() {
+  city.classList.toggle("active");
   Array.from(cont).forEach((el) => {
-    el.style.display = "block";
+    el.style.display = el.style.display === "block" ? "none" : "block";
   });
 }
